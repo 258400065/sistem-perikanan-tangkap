e6dddfe248533179b71620525747c0f16c743585
DUBIR AVUT <258400065@students.uma.ac.id>
2025-11-17 13:05:58 +0700

    Create sidebar.php file in admin/includes directory

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,31 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard.php">
+                    <i class="bi bi-speedometer2"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="bbm/index.php">
+                    <i class="bi bi-fuel-pump"></i> Data BBM
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="files/index.php">
+                    <i class="bi bi-file-earmark"></i> Kelola File
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="sop/index.php">
+                    <i class="bi bi-clipboard-check"></i> Kelola SOP
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="users/index.php">
+                    <i class="bi bi-people"></i> Kelola User
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
