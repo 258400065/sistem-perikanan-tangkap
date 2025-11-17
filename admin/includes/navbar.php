a39118eefff79b840fa185212e0cf596cb71517b
DUBIR AVUT <258400065@students.uma.ac.id>
2025-11-17 13:04:54 +0700

    Add admin navbar component

---
+++ admin/includes/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-dark bg-dark sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="dashboard.php">
+            <i class="bi bi-water"></i> Admin Perikanan Tangkap
+        </a>
+        <div class="d-flex">
+            <a href="../index.php" class="btn btn-outline-light btn-sm me-2" target="_blank">
+                <i class="bi bi-eye"></i> Lihat Website
+            </a>
+            <a href="logout.php" class="btn btn-outline-danger btn-sm">
+                <i class="bi bi-box-arrow-right"></i> Logout
+            </a>
+        </div>
+    </div>
+</nav>
